86fc3f409d9eff3330b69d3e51cedf0b9eb9dc97
pantaparna <98857439+pantaparna@users.noreply.github.com>
2024-02-24 23:30:22 -0500

    Update

--- flu_data.sql
+++ flu_data.sql
@@ -1,6 +1,6 @@
 
 /* 
-Obejectives: come up with flu shot dashboard for 2022 that does the following
+Objectives: come up with a flu shot dashboard for 2022 that does the following
 
 1) Total number of patients getting flu shots stratified by
 a)Age
@@ -8,20 +8,29 @@ b)Race
 c)County
 d)Overall
 
-2)Runnig total of flu shot over the course of 2022
+2)Running total of flu shots for 2022
 3)Total number of flu shots given in 2022
-4)A list of patients that show whether or not they recieve a flue shot
+4)A list of patients that show whether or not they receive a flu shot
 
 Requirement
-Patient must have been 'Active at our hospital'.
+The patient must have been 'Active at our hospital'.
 */
 
 # Use JOINS AND CTE to extract data of interest
-# Export data in a csv file format for further analysis using Tableau       
+# Export data in a CSV file format for further analysis using Tableau       
 
 
 
-with flu_shot_2022 as
+WITH active_patients as
+(SELECT distinct patient
+FROM Hospital.encounters e
+JOIN patients pat
+ON pat.id = e.patient
+where e.start between '2020-01-01 00:00' and '2022-12-31 23:59'
+AND pat.deathdate is NULL
+AND TIMESTAMPDIFF(YEAR, pat.birthdate, CURDATE()) >=6), # Age filter for age > 6
+
+flu_shot_2022 as
 (SELECT Patient, min(date) as earliest_flu_date 
 FROM immunizations
 where CODE = '5302'
@@ -44,8 +53,11 @@ SELECT TIMESTAMPDIFF(YEAR, pat.birthdate, CURDATE()) AS age,
        end as flu_shot_2022
 FROM patients as pat
 Left Join flu_shot_2022 as flu
-on pat.id = flu.patient;
+on pat.id = flu.patient
+where 1=1
+and pat.id in (select patient from active_patients);
        
+
 
 
 
